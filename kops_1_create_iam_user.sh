7d1e286966257594570f13027f33dfd7da42858e
Gary Tay <garyttt@singnet.com.sg>
2020-07-12 20:37:59 +0800

    script 1: automate kops aws profile creation

--- kops_1_create_iam_user.sh
+++ kops_1_create_iam_user.sh
@@ -17,7 +17,7 @@ aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AmazonVPCFullAc
 aws iam create-user --user-name $AWS_USR
 aws iam get-user --user-name $AWS_USR
 aws iam add-user-to-group --user-name $AWS_USR --group-name $AWS_GRP
-aws iam create-access-key --user-name $AWS_USR
+aws iam create-access-key --user-name $AWS_USR | tee tmp$$.log
 
 echo "Please copy and paste the ACCESS_KEY_ID and SECRET_ACCESS_KEY to your AWS PROFILE"
 echo "Example: paste the following lines to ~/.aws/credentials"
@@ -28,3 +28,14 @@ aws_access_key_id = ACCESS_KEY_ID
 aws_secret_access_key = SECRET_ACCESS_KEY  
 
 EOF
+
+echo "Automating the above: creating kops AWS profile..."
+AKI=`cat tmp$$.log | jq '.AccessKey.AccessKeyId' | tr -d \"`
+SAK=`cat tmp$$.log | jq '.AccessKey.SecretAccessKey' | tr -d \"`
+aws configure --profile=kops set aws_access_key_id $AKI
+aws configure --profile=kops set aws_secret_access_key $SAK
+aws configure --profile=kops set region ap-southeast-1
+aws configure --profile=kops set output json
+
+# Make sure you clean-up log containing sensitive credentials
+rm -f tmp$$.log
