6e3e5778bcafc674e305e3f0ee8dac800c9fc4f8
Gary Tay <garyttt@singnet.com.sg>
2020-07-13 19:53:29 +0800

    Minor changes

--- kops_1_create_iam_user.sh
+++ kops_1_create_iam_user.sh
@@ -17,9 +17,9 @@ aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AmazonVPCFullAc
 aws iam create-user --user-name $AWS_USR
 aws iam get-user --user-name $AWS_USR
 aws iam add-user-to-group --user-name $AWS_USR --group-name $AWS_GRP
-aws iam create-access-key --user-name $AWS_USR | tee tmp$$.log
+aws iam create-access-key --user-name $AWS_USR | tee ~/.aws/tmp$$.log
 
-echo "Please copy and paste the ACCESS_KEY_ID and SECRET_ACCESS_KEY to your AWS PROFILE"
+echo "Please copy and paste the ACCESS_KEY_ID and SECRET_ACCESS_KEY as shown above to your AWS PROFILE for ${AWS_USR}"
 echo "Example: paste the following lines to ~/.aws/credentials"
 echo 
 cat <<EOF >&1
@@ -29,13 +29,14 @@ aws_secret_access_key = SECRET_ACCESS_KEY
 
 EOF
 
-echo "Automating the above: creating kops AWS profile..."
-AKI=`cat tmp$$.log | jq '.AccessKey.AccessKeyId' | tr -d \"`
-SAK=`cat tmp$$.log | jq '.AccessKey.SecretAccessKey' | tr -d \"`
+echo "Automating the above: creating ${AWS_USR} AWS profile..."
+AKI=`cat ~/.aws/tmp$$.log | jq '.AccessKey.AccessKeyId' | tr -d \"`
+SAK=`cat ~/.aws/tmp$$.log | jq '.AccessKey.SecretAccessKey' | tr -d \"`
 aws configure --profile=kops set aws_access_key_id $AKI
 aws configure --profile=kops set aws_secret_access_key $SAK
 aws configure --profile=kops set region ap-southeast-1
 aws configure --profile=kops set output json
 
-# Make sure you clean-up log containing sensitive credentials
-rm -f tmp$$.log
+# Make sure you clean-up log containing sensitive credentials, and protect it
+rm -f ~/.aws/tmp$$.log 
+chmod 600 ~/.aws/credentials
